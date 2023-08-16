033a8b771cc72826ea01dbe3df88da2339119ff9
Sajan Toor <sajantoor98@gmail.com>
2023-08-15 21:53:51 -0700

    Handle UDP packet

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use etherparse::Ipv4HeaderSlice;
+use etherparse::{Ipv4HeaderSlice, UdpHeaderSlice};
 use std::io;
 use tun_tap::{Iface, Mode};
 
@@ -12,12 +12,13 @@ fn main() -> io::Result<()> {
 
         // By tuntap docs: https://docs.kernel.org/networking/tuntap.html
         // the first 2 bytes are flags and the next 2 bytes are protocol
-        let flags = u16::from_be_bytes([buffer[0], buffer[1]]);
+        // let flags = u16::from_be_bytes([buffer[0], buffer[1]]);
         let ether_protocol = u16::from_be_bytes([buffer[2], buffer[3]]);
         let udp_protocol = 17;
 
         // After that is the raw protocol(IP, IPv6, etc) frame.
         // Ignore any non-IPv4 packets
+        // TODO: Support ipv6 as well
         if ether_protocol != ipv4_protocol {
             continue;
         }
@@ -58,6 +59,54 @@ fn main() -> io::Result<()> {
     }
 }
 
-fn handle_udp_packet(buffer: &[u8]) {
-    println!("UDP Packet: {:?}", buffer);
+fn handle_udp_packet(packet: &[u8]) {
+    // UDP header is 8 bytes
+    let header = match UdpHeaderSlice::from_slice(&packet[..8]) {
+        Err(e) => {
+            println!("Got bad UDP packet: {}", e);
+            return;
+        }
+
+        Ok(h) => h,
+    };
+
+    // RFC 768 specification of UDP header
+    // 0      7 8     15 16    23 24    31
+    // +--------+--------+--------+--------+
+    // |     Source      |   Destination   |
+    // |      Port       |      Port       |
+    // +--------+--------+--------+--------+
+    // |                 |                 |
+    // |     Length      |    Checksum     |
+    // +--------+--------+--------+--------+
+    // |
+    // |          data octets ...
+    // +---------------- ...
+
+    let checksum_value = header.checksum();
+
+    // TODO: Validate checksum, this header.checksum() function seems wrong
+
+    let udp_length = header.length();
+    let source_port = header.source_port();
+    let destination_port = header.destination_port();
+
+    println!(
+        "UDP Packet: {} -> {}; Length: {}b Checksum: {}",
+        source_port, destination_port, udp_length, checksum_value
+    );
+
+    // Read the data from the packet
+    let data = &packet[8..udp_length as usize];
+
+    // Parse the data as a string
+    let data_string = match std::str::from_utf8(data) {
+        Ok(v) => v,
+        Err(e) => {
+            println!("Invalid UTF-8 sequence: {}", e);
+            return;
+        }
+    };
+
+    println!("Data: {}", data_string);
 }
